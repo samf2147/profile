9727856545b1a385eeef9ce1167f1653cd68fe56
Sam Frank <sfrank2147@gmail.com>
2014-01-05 14:20:42 -0600

    removed messaging route

--- router.py
+++ router.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 @app.route('/home/')
 def home():
     return render_template('home.html')
-@app.route('/message/<name>/')
-def message(name):
-    return render_template('name.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
